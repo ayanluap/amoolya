24ead3ffbe04321a5c86d2ffddae76f420118881
ayanluap <ayanluap@gmail.com>
2021-01-17 16:57:04 +0530

    f complete

--- frontend/src/components/pages/Contact.js
+++ frontend/src/components/pages/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '../layouts/Layout';
+import logo from '../../assets/logo.png';
 
 const Contact = () => {
   return (
@@ -9,66 +10,77 @@ const Contact = () => {
           <p className="about__text container prime-heading">Contact Us</p>
         </div>
       </div>
+      <div className="contact__container container">
+        <form className="form">
+          <div className="form__collection container">
+            <p className="u-text u-mb-2">
+              Please fill the given form to contact!
+            </p>
+            <div className="form__group">
+              <label htmlFor="fullname" className="form__label">
+                name
+              </label>
+              <input
+                type="text"
+                name="fullname"
+                id="fullname"
+                className="form__input"
+                placeholder="Full Name"
+              />
+            </div>
+            <div className="form__group">
+              <label htmlFor="email" className="form__label">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="form__input"
+                placeholder="Email id"
+              />
+            </div>
+            <div className="form__group">
+              <label htmlFor="phone" className="form__label">
+                Phone
+              </label>
+              <input
+                type="number"
+                name="phone"
+                id="phone"
+                className="form__input"
+                placeholder="Phone number"
+              />
+            </div>
+            <div className="form__group">
+              <label htmlFor="msg" className="form__label">
+                message
+              </label>
+              <textarea
+                name="msg"
+                id="msg"
+                cols="30"
+                rows="10"
+                className="form__input"
+                placeholder="enter your message"
+              ></textarea>
+            </div>
+            <button type="submit" className="btn btn--blue">
+              submit
+            </button>
+          </div>
+        </form>
 
-      <form className="form">
-        <div className="form__collection container">
-          <p className="u-text u-mb-2">
-            Please fill the given form to contact!
+        <div className="address">
+          <div className="address__img">
+            <img src={logo} alt="logo" />
+          </div>
+          <p className="address__text u-text">
+            KLJ Tower, 9th floor, 903, Netaji Subhash Place
           </p>
-          <div className="form__group">
-            <label htmlFor="fullname" className="form__label">
-              name
-            </label>
-            <input
-              type="text"
-              name="fullname"
-              id="fullname"
-              className="form__input"
-              placeholder="Full Name"
-            />
-          </div>
-          <div className="form__group">
-            <label htmlFor="email" className="form__label">
-              Email
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className="form__input"
-              placeholder="Email id"
-            />
-          </div>
-          <div className="form__group">
-            <label htmlFor="phone" className="form__label">
-              Phone
-            </label>
-            <input
-              type="number"
-              name="phone"
-              id="phone"
-              className="form__input"
-              placeholder="Phone number"
-            />
-          </div>
-          <div className="form__group">
-            <label htmlFor="msg" className="form__label">
-              message
-            </label>
-            <textarea
-              name="msg"
-              id="msg"
-              cols="30"
-              rows="10"
-              className="form__input"
-              placeholder="enter your message"
-            ></textarea>
-          </div>
-          <button type="submit" className="btn btn--blue">
-            submit
-          </button>
+          <p className="u-badge u-text">Pitam Pura, Delhi, 110034</p>
         </div>
-      </form>
+      </div>
     </Layout>
   );
 };
